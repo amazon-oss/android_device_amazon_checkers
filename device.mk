6ae173413b1b685b2ce4235580d4b367f01c5e42
R0rt1z2 <me@r0rt1z2.com>
2026-05-13 12:32:31 +0200

    checkers: Import device specific init script
    Change-Id: Iaad12a330a700cd85c3737209bfda2cee44bf7f2

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ PRODUCT_CHARACTERISTICS := tablet,nosdcard
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.device.rc
+
 # Inherit from mt8163-common
 $(call inherit-product-if-exists, device/amazon/mt8163-common/mt8163-common.mk)
 
